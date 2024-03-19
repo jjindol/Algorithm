63548c9465f2ef7dab9d158a962928e5c65e8d50
jjindol <andy15948@naver.com>
2024-03-19 23:34:53 +0900

    최댓값

--- BAEKJOON/04_1차원배열/04.py
+++ BAEKJOON/04_1차원배열/04.py
@@ -1,11 +1,7 @@
-max_value = 0
-max_index = 0
-
-for i in range(1, 10):
-    num = int(input())
-    if num > max_value:
-        max_value = num
-        max_index = i
-
-print(max_value)
-print(max_index)
+num = []
+for i in range(9):
+	x = int(input())
+	num.append(x)
+	
+print(max(num))
+print(num.index(max(num))+1)
