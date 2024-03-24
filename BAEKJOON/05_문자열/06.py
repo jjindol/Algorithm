8e5f3a9b847f605b6d110b47f83d045c2f8c3c72
jjindol <andy15948@naver.com>
2024-03-24 17:57:40 +0900

    알파벳 찾기

--- BAEKJOON/05_문자열/06.py
+++ BAEKJOON/05_문자열/06.py
@@ -1,5 +1,26 @@
-word = input()
-alphabet = [chr(j) for j in range(97,123)]
+# 방법1
+s = input()
+alphabet = [i for i in range(97,123)]
 
 for x in alphabet:
-    print(word.find(x), end=' ')
+    print(s.find(chr(x)), end=" ")
+
+
+# 방법2
+s = input()
+alphabet = "abcdefghijklmnopqrstuvwsyz"
+
+for x in alphabet:
+    print(s.find(x), end=" ")
+
+
+# 방법3
+my_list = [-1] * 26
+s = list(map(ord, input()))
+
+for i in range(len(s)):
+    if my_list[s[i]-ord('a')]== -1:
+        my_list[s[i]-ord('a')]= i
+
+for i in my_list:
+    print(i, end=" ")
