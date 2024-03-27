d5389ae5a798ac5169ef7d518b94aa83abf07f9f
jjindol <andy15948@naver.com>
2024-03-27 19:14:09 +0900

    별 찍기-2

--- BAEKJOON/03_반복문/10.py
+++ BAEKJOON/03_반복문/10.py
@@ -1,4 +1,7 @@
-n = int(input())
+N = int(input())
 
-for i in range(1, n+1):
-    print(" " * (n-i) + "*" * i)
+for i in range(N):
+    print(" "*(N-i-1) + "*"*(i+1))
+
+# + 연산자는 공백과 별을 하나의 문자열로 연결하여 출력
+# , 는 print 함수에 전달되는 인자 구분
