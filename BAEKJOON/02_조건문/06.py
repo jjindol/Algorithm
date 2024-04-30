7032a430aa0edbb0f6cc185d079c58b6ba6f1357
jjindol <andy15948@naver.com>
2024-04-30 22:06:44 +0900

    오븐 시계

--- BAEKJOON/02_조건문/06.py
+++ BAEKJOON/02_조건문/06.py
@@ -4,6 +4,6 @@ C = int(input())
 if B+C < 60:
     print(A, B+C)
 elif A + (B+C)//60 >= 24:
-    print((A + (B+C)//60) % 24, (B+C) % 60)
+    print((A + (B+C)//60) - 24, (B+C) % 60)
 else:
     print(A + (B+C)//60, (B+C) % 60)
