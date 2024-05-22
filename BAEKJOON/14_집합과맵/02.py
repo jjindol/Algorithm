73715d373ea976a1430c143b4a9bf817e4ed7c86
jjindol <andy15948@naver.com>
2024-05-23 01:05:14 +0900

    문자열 집합

--- BAEKJOON/14_집합과맵/02.py
+++ BAEKJOON/14_집합과맵/02.py
@@ -1,45 +1,21 @@
-#풀이1
-import sys
-input = sys.stdin.readline
+n,m = map(int, input().split())
+my_set = set()
+lst = []
 
-n = int(input())
-my_card = set(map(int, input().split()))
-m = int(input())
-card = list(map(int, input().split()))
+for _ in range(n):
+    word = input()
+    my_set.add(word)
 
-for x in range(m):
-    if card[x] in my_card:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+for _ in range(m):
+    word_test = input()
+    lst.append(word_test)
 
+count = 0
+for word_test in lst:
+    if word_test in my_set:
+        count += 1
+print(count)
 
-'''
-풀이2
-import sys
-
-def binary_search(array, check, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == check:
-            return True
-        elif array[mid] > check:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return False
-
-input = sys.stdin.readline
-
-N = int(input())
-your_card = sorted(list(map(int, input().rstrip().split())))
-
-M = int(input())
-compare_card = list(map(int, input().rstrip().split()))
-
-for num in compare_card:
-    if binary_search(your_card, num, 0, N-1):
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
-'''
+"""
+N개의 집합에 대해 M개의 입력 중 몇 개가 N개에 속하는 지 구하는 문제로 N개는 집합으로 중복된 것이 있을 수 없지만 입력 M에 대해서는 중복이 있을 수 있기에 같은 것이 입력되면 그 만큼 정답의 개수에 추가에 주어야 합니다. 따라서 코드에서 b를 set으로 선언하신 것은 올바르지 않은 방법입니다.
+"""
