c41edc60d09067bc0696ad23e762dc274c0afba2
jjindol <andy15948@naver.com>
2024-08-01 14:40:25 +0900

    Implementation

---
+++ python-for-coding-test/chapter04/4-3.py
@@ -0,0 +1,13 @@
+command = input()
+row = int(command[1])
+column = int(ord(command[0]) - ord('a') + 1)
+
+steps = [(-2,-1), (-2,1), (2,1), (2,-1), (1,2), (1,-2), (-1,2), (-1,-2)]
+
+result = 0
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+print(result)
