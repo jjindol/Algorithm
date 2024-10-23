1aa6ab75a15d0142ae091c30ff67722f95fd8e0d
jjindol <andy15948@naver.com>
2024-10-23 21:27:58 +0900

    블랙잭

--- BAEKJOON/12_브루트포스/01.py
+++ BAEKJOON/12_브루트포스/01.py
@@ -1,9 +1,12 @@
-from itertools import combinations
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 nums = list(map(int, input().split()))
-results = 0
 
-for cards in combinations(nums,3):
-    if sum(cards) <= m:
-        results = max(results,sum(cards))
-print(results)
+result = 0
+for i in range(0,n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            tmp = nums[i] + nums[j] + nums[k]
+            if tmp <= m:
+                result = max(result, tmp)
+
+print(result)
