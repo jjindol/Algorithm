b6b4d3b57a1649cb1c55aa87ac7484e7b2424ccb
jjindol <andy15948@naver.com>
2024-11-08 23:58:58 +0900

    Implementation

--- python-for-coding-test/chapter04/4-1.py
+++ python-for-coding-test/chapter04/4-1.py
@@ -2,17 +2,19 @@ n = int(input())
 x,y = 1,1
 plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+#L,R,U,D에 따른 이동 방향
+dx,dy = [0,0,-1,1], [-1,1,0,0]
 move_types = ['L','R','U','D']
 
+#이동 계획 하나씩 확인
 for plan in plans:
-    for i in range(4):
+    for i in range(len(move_types)):
         if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
+            nx,ny = x+dx[i], y+dy[i]
+
+    if nx < 1 or ny < 1 or nx > n or  ny > n:
         continue
+
     x,y = nx,ny
 
-print(nx,ny)
+print(x,y)
