449c57984fead3a5fb6a26e8e90ecb7817c14d57
jjindol <andy15948@naver.com>
2024-11-21 23:26:37 +0900

    소수 만들기

---
+++ Programmers/Lv1/12977.py
@@ -0,0 +1,30 @@
+import math
+
+def solution(nums):
+    answer = 0
+    lst = []
+    N = len(nums)
+
+    for i in range(N-2):
+        for j in range(i+1,N-1):
+            for k in range(j+1,N):
+                tmp = nums[i] + nums[j] + nums[k]
+                lst.append(tmp)
+
+    for i in range(len(lst)):
+        if isPrime(lst[i]):
+            answer += 1
+
+    return answer
+
+
+def isPrime(x):
+    if x <= 1:
+        return False
+
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+print(solution([1,2,7,6,4]))
